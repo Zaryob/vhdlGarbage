ec641b794e64461f2ca418b18122aa1a33cdaa07
Suleyman Poyraz <zaryob.dev@gmail.com>
2020-11-06 01:06:25 +0300

    Project 5 added.

--- README.md
+++ README.md
@@ -12,3 +12,4 @@ Contents
 * [Project 2](https://github.com/Zaryob/vhdlGarbage/tree/master/project_2): a basic logic operator and inverter example extended with vector form.
 * [Project 3](https://github.com/Zaryob/vhdlGarbage/tree/master/project_3): another basic logical operation example supported with simulations.
 * [Project 4](https://github.com/Zaryob/vhdlGarbage/tree/master/project_4): xilinx logic device library added project example
+* [Project %](https://github.com/Zaryob/vhdlGarbage/tree/master/project_5): xilinx logic device library added project example 2
---
+++ project_5/project_5.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -0,0 +1,37 @@
+--Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+----------------------------------------------------------------------------------
+--Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+--Date        : Sat Oct 31 13:13:51 2020
+--Host        : DESKTOP-H1K4L7I running 64-bit major release  (build 9200)
+--Command     : generate_target design_1_wrapper.bd
+--Design      : design_1_wrapper
+--Purpose     : IP block netlist
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+entity design_1_wrapper is
+  port (
+    A : in STD_LOGIC;
+    B : in STD_LOGIC;
+    E : out STD_LOGIC
+  );
+end design_1_wrapper;
+
+architecture STRUCTURE of design_1_wrapper is
+  component design_1 is
+  port (
+    A : in STD_LOGIC;
+    B : in STD_LOGIC;
+    E : out STD_LOGIC
+  );
+  end component design_1;
+begin
+design_1_i: component design_1
+     port map (
+      A => A,
+      B => B,
+      E => E
+    );
+end STRUCTURE;
